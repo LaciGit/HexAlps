158210bb05b5c4c4edc380b4d385b9ed17b1b928
LaciGit <laszloxd@gmail.com>
2024-11-09 13:10:40 +0100

    simple tile selection

--- src/game_manager/game_manager.gd
+++ src/game_manager/game_manager.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name GameManager
 
+# Tilemap
+@export var hexa_tile_layer_0: HexaTileLayer
+
 # Player
 ## resources
 var path_player: PackedScene = preload("res://scenes/characters/player.tscn")
@@ -9,7 +12,8 @@ var player: Player = null
 # starting hex for player
 
 func _ready() -> void:
-
-
-    if not player:
-        var tmp_player = path_player.instantiate()
+	if not player:
+		var tmp_player = path_player.instantiate()
+		var local_map_pos = hexa_tile_layer_0.map_to_local(Vector2i(0, 0))
+		tmp_player.position = local_map_pos
+		add_child(tmp_player)
